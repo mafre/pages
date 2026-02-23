138c241f41206270316621e6cc4557eb5b3bbf65
Mathias Fredriksson <mathiasfr@gmail.com>
2026-02-23 09:37:59 +0100

    config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
+  output: "standalone",
   basePath: process.env.PAGES_BASE_PATH,
 };
 
